d74059b242ee93cbcdef5df592b79b0ba9505797
louis <asimhi.louis@gmail.com>
2021-03-27 17:15:57 +0100

    9

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var $ = require('jquery')
 app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(express.static("public"));
-app.use(express.static(__dirname + "/public"))
+app.use(express.static("public"));
+// app.use(express.static(__dirname + "/public"))
 
 
 
