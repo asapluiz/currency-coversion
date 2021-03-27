6ddf6fa54f95519b1b07b18b66ea5c0bbd465de2
louis <asimhi.louis@gmail.com>
2021-03-27 17:36:27 +0100

    14

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const https = require("https");
 const axios = require("axios");
 var $ = require('jquery')
 
-aaaa
 app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-// app.use(express.static(__dirname + "/public"))
+
 
 
 
